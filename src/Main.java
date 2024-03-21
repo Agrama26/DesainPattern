122041e1c9be59b1a57a04159aa8185bb157f3b3
Agrama26 <ramadhanagung1112@gmail.com>
2024-03-21 09:33:52 +0700

    Duck tahap 3 menggunakan interface dan mulai menggunakan strategy pattern
    Signed-off-by: Agrama26 <ramadhanagung1112@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-//        Order order1 = new Order(30); // Total harga pesanan $30
-//
-//        order1.setShippingStrategy(new StandardShipping());
-//        System.out.println("Biaya Pengiriman Standar: $" + order1.calculateShippingCost());
-//
-//        order1.setShippingStrategy(new ExpressShipping());
-//        System.out.println("Biaya Pengiriman Ekspres: $" + order1.calculateShippingCost());
-//
-//        order1.setShippingStrategy(new FreeShipping());
-//        System.out.println("Biaya Pengiriman Gratis: $" + order1.calculateShippingCost());
 
-//        Player yohsimitsu = new Player();
-//        yohsimitsu.pushCross("flying kick");
-//        yohsimitsu.pushTrianlge("sword");
-//        Player paul = new Player();
-//        paul.pushCross("lower kick");
-//        paul.pushTrianlge("uppercut");
+        System.out.println("Jenis Jenis Serangan : ");
         Player yoshimitsu = new Player();
         yoshimitsu.pushCross(new FlyingKick());
         yoshimitsu.pushTrianlge(new SwordPunch());
@@ -27,13 +12,19 @@ public class Main {
         paul.pushCross(new LowerKick());
         paul.pushTrianlge(new UppercutPunch());
 
+        System.out.println("\nJenis Jenis Bebek : ");
         Duck mallard = new MallardDuck();
         mallard.performQuack();
         mallard.performFly();
+        mallard.swim();
+        mallard.display();
 
+        System.out.println("\nBebek 2 : ");
         Duck modelduck = new ModelDuck();
         modelduck.performFly(); // Akan memanggil contructor modelduck(FlyNoway)
         modelduck.setFlyBehavior((new FlyWithRocketPower()));
         modelduck.performFly();
+
+
     }
 }
